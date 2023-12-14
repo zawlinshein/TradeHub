75c33822c03072ef924aacc9121caff28fa359a5
zawlinshein <pudgeismyfavourate@gmail.com>
2023-12-15 00:22:48 +0630

    first initalize

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  "plugins": [
+    ["module-resolver", {
+      "root": ["./src"],
+      "alias": {
+        "@": "./src",
+        "components": "./src/components",
+        "assets": "./src/assets",
+        "navigations": "./src/navigations",
+        "screens": "./src/screens",
+      }
+    }]
+  ]
 };
